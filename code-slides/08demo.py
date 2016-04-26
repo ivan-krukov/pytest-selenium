97f84766e4f66b1a3d7be57d5a92e7662c4430c2
meejah <meejah@meejah.ca>
2016-04-26 09:09:48 -0600

    add cheatsheet of filenames to open

--- code-slides/08demo.py
+++ code-slides/08demo.py
@@ -1,5 +1,6 @@
 ## <h1 style="valign:center;">Demo</h1>
 ## <ul class="incremental">
+##   <li><tt>test_fixturedemo[012].py, test_github.py, conftest.py</tt></li>
 ##   <li>Funtimes command-line options: <span class="incremental"><tt>-v</tt><span>, <tt>-s</tt></span></span></li>
 ##   <li><tt>@fixture</tt>: <span class="incremental">cleanup, <span>scoping, </span><span>combinations</span></span></li>
 ##   <li>Parametrizing: <span class="incremental">fixtures vs <span>tests</span></span></li>
